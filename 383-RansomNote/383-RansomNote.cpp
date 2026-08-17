62f82e7173d966ac8b920ccc31157d680c666817
Aksh Patel <128889808+akshpatel10@users.noreply.github.com>
2026-08-17 09:29:55 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.cpp
@@ -0,0 +1,20 @@
+// Last updated: 17/08/2026, 09:29:55
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> vec(26, 0);
+
+        for (int i = 0; i < magazine.size(); i++) {
+            vec[magazine[i] - 'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.size(); i++) {
+
+            if (vec[ransomNote[i] - 'a'] == 0)
+                return false;
+
+            vec[ransomNote[i] - 'a']--;
+        }
+        return true;
+    }
+};
