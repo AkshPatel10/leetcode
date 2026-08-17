0a4a752125fedc10a57ff19ca0164a025b0b0c5e
Aksh Patel <128889808+akshpatel10@users.noreply.github.com>
2026-08-17 09:29:31 +0530

    Create 561-ArrayPartition

---
+++ 561-ArrayPartition/561-ArrayPartition.cpp
@@ -0,0 +1,12 @@
+// Last updated: 17/08/2026, 09:29:31
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int result = 0;
+        for (int i = 0; i < nums.size(); i += 2) {
+            result += nums[i];
+        }
+        return result;
+    }
+};
