97e0f54fc41d6b24a4bc653507467401a92d34c2
Aksh Patel <128889808+akshpatel10@users.noreply.github.com>
2026-08-17 09:30:05 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.cpp
@@ -0,0 +1,23 @@
+// Last updated: 17/08/2026, 09:30:04
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> result(n+1);
+
+        if(n == 0){
+            return result;
+        }
+
+        result[0] = 0;
+        
+        for(int i = 0; i < n+1; i++){
+            if(i%2 != 0){
+                result[i] = result[i/2]+1;
+            }
+            else{
+                result[i] = result[i/2];
+            }
+        }
+        return result;
+    }
+};
