877a34d2b023a0bb37fd23ef1f6aa89d9f5d1421
Aksh Patel <128889808+akshpatel10@users.noreply.github.com>
2026-08-17 09:25:23 +0530

    Create 2743-Debounce

---
+++ 2743-Debounce/2743-Debounce.js
@@ -0,0 +1,25 @@
+// Last updated: 17/08/2026, 09:25:23
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timer;
+    
+     return function(...args) {
+
+        clearTimeout(timer);
+
+        timer = setTimeout(() => {
+            fn(...args);
+        }, t);
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
