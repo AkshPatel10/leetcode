ad86d22c67c27ad72d9e36a3add2856f95809409
Aksh Patel <128889808+akshpatel10@users.noreply.github.com>
2026-08-17 09:28:41 +0530

    Create 1260-DayOfTheYear

---
+++ 1260-DayOfTheYear/1260-DayOfTheYear.cpp
@@ -0,0 +1,27 @@
+// Last updated: 17/08/2026, 09:28:40
+class Solution {
+public:
+    int dayOfYear(string date) {
+
+        int year = stoi(date.substr(0,4));
+        int month = stoi(date.substr(5,2));
+        int day = stoi(date.substr(8,2));
+
+        vector<int> days = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+        // check leap year
+        if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+            days[1] = 29;
+        }
+
+        int total = 0;
+
+        for(int i = 0; i < month - 1; i++){
+            total += days[i];
+        }
+
+        total += day;
+
+        return total;
+    }
+};
