0f9e250c09db602544cb47c546340556a740b358
Aksh Patel <128889808+akshpatel10@users.noreply.github.com>
2026-08-17 09:28:50 +0530

    Create 1140-DistantBarcodes

---
+++ 1140-DistantBarcodes/1140-DistantBarcodes.cpp
@@ -0,0 +1,36 @@
+// Last updated: 17/08/2026, 09:28:49
+class Solution {
+public:
+    vector<int> rearrangeBarcodes(vector<int>& barcodes) {
+
+        unordered_map<int, int> freq;
+        for (int x : barcodes) {
+            freq[x]++;
+        }
+
+        vector<pair<int, int>> arr;
+        for (auto x : freq) {
+            arr.push_back({x.second, x.first});
+        }
+
+        sort(arr.rbegin(), arr.rend());
+        vector<int> ans(barcodes.size());
+        int index = 0;
+
+        for (auto x : arr) {
+            int count = x.first;
+            int barcode = x.second;
+
+            while (count--) {
+                ans[index] = barcode;
+
+                index += 2;
+
+                if (index >= barcodes.size()) {
+                    index = 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
