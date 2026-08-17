cbeec590d58175928d79cec346668e8ec8f74486
Aksh Patel <128889808+akshpatel10@users.noreply.github.com>
2026-08-17 09:26:22 +0530

    Create 2102-FindTheMiddleIndexInArray

---
+++ 2102-FindTheMiddleIndexInArray/2102-FindTheMiddleIndexInArray.cpp
@@ -0,0 +1,23 @@
+// Last updated: 17/08/2026, 09:26:22
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int sum = 0;
+        int rightSum = 0;
+        int leftSum = 0;
+
+        for(int num : nums){
+            sum += num;
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            rightSum = sum - leftSum - nums[i];
+            if(leftSum == rightSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
