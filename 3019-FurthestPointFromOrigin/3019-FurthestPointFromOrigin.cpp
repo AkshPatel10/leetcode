517bafe6a6ebbf908fdb68eafd18aaf20eef9050
Aksh Patel <128889808+akshpatel10@users.noreply.github.com>
2026-08-17 09:25:03 +0530

    Create 3019-FurthestPointFromOrigin

---
+++ 3019-FurthestPointFromOrigin/3019-FurthestPointFromOrigin.cpp
@@ -0,0 +1,17 @@
+// Last updated: 17/08/2026, 09:25:03
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int left = 0;
+        int right = 0;
+        int dash = 0;
+
+        for(char &ch: moves){
+            if(ch == 'L') left++;
+            else if(ch == 'R') right++;
+            else dash++;
+        }
+
+        return abs(left - right) + dash;
+    }
+};
