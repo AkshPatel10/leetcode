d06f7ac8e56f12ef2464ca242e88599376eb3fdd
Aksh Patel <128889808+akshpatel10@users.noreply.github.com>
2026-08-17 09:29:59 +0530

    Create 349-IntersectionOfTwoArrays

---
+++ 349-IntersectionOfTwoArrays/349-IntersectionOfTwoArrays.cpp
@@ -0,0 +1,18 @@
+// Last updated: 17/08/2026, 09:29:59
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+        unordered_set<int> s(nums1.begin(), nums1.end());
+        vector<int> result;
+
+        for(int num : nums2){
+            if(s.count(num)){
+                result.push_back(num);
+                s.erase(num);
+            }
+        }
+
+        return result;
+    }
+};
