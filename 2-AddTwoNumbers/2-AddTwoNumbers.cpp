8727307b63b5da8e47283eecab7ca4d5502ee34e
Aksh Patel <128889808+akshpatel10@users.noreply.github.com>
2026-08-17 09:32:52 +0530

    Create 2-AddTwoNumbers

---
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cpp
@@ -0,0 +1,31 @@
+// Last updated: 17/08/2026, 09:32:51
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);          // dummy node
+        ListNode* temp = &dummy;    // pointer to build result
+        
+        int carry = 0;
+
+        while(l1 != NULL || l2 != NULL || carry != 0){
+            int sum = carry;
+
+            if(l1 != NULL){
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if(l2 != NULL){
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+
+            temp->next = new ListNode(sum % 10);
+            temp = temp->next;
+        }
+
+        return dummy.next;
+    }
+};
