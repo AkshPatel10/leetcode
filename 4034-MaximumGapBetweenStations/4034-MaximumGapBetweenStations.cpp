e5755bfa067c3b5b38c890af62483de656cc6efb
Aksh Patel <128889808+akshpatel10@users.noreply.github.com>
2026-08-17 09:23:11 +0530

    Create 4034-MaximumGapBetweenStations

---
+++ 4034-MaximumGapBetweenStations/4034-MaximumGapBetweenStations.cpp
@@ -0,0 +1,42 @@
+// Last updated: 17/08/2026, 09:23:10
+class Solution {
+public:
+    int maximumGap(string skill, string station) {
+        int n = skill.size();
+        int m = station.size();
+
+        if(n == 1)
+            return 0;
+
+        vector<int> earliest(n), latest(n);
+
+        int pos = -1;
+        for(int i = 0; i < n; i++){
+            pos++;
+            while (pos < m && station[pos] != skill[i]){
+                pos++;
+            }
+            earliest[i] = pos;
+        }
+        pos = m-1;
+
+        for(int i = n-1; i >= 0; i--){
+            while(pos >= 0 && station[pos] != skill[i]){
+                pos--;
+            }
+            latest[i] = pos;
+            pos--;
+        }
+
+        int ans = 0;
+
+        for(int i = 1; i < n; i++){
+            int gap = latest[i] - earliest[i-1];
+
+            if(gap>ans){
+                ans = gap;
+            }
+        }
+        return ans;
+    }
+};
