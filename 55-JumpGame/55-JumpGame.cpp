7ee693e3f56eb1e6cdf1bd79794114472dd80778
Aksh Patel <128889808+akshpatel10@users.noreply.github.com>
2026-08-17 09:31:58 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.cpp
@@ -0,0 +1,14 @@
+// Last updated: 17/08/2026, 09:31:58
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal = nums.size()-1;
+
+        for(int i = goal-1; i>=0; i--){
+            if(nums[i]+i >= goal){
+                goal = i;
+            }
+        }
+        return goal == 0;
+    }
+};
