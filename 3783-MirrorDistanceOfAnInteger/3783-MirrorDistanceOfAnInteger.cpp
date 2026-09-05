b47edc0f73d53008fbb58325ec648ab1225fc067
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-05 10:06:26 +0530

    Create 3783-MirrorDistanceOfAnInteger

---
+++ 3783-MirrorDistanceOfAnInteger/3783-MirrorDistanceOfAnInteger.cpp
@@ -0,0 +1,16 @@
+// Last updated: 05/09/2026, 10:06:26
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        int original = n;
+        int reverse = 0;
+
+        while(n > 0){
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n = n / 10;
+        }
+
+        return abs(original - reverse);
+    }
+};
