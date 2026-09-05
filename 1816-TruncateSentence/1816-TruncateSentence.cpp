961df53c1e77e39eebe3499b14bfc5d91813b3fa
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-05 10:10:49 +0530

    Create 1816-TruncateSentence

---
+++ 1816-TruncateSentence/1816-TruncateSentence.cpp
@@ -0,0 +1,17 @@
+// Last updated: 05/09/2026, 10:10:48
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int space = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == ' '){
+                space++;
+            }
+            if(space == k){
+                return s.substr(0,i);
+            }
+        }
+        return s;
+    }
+};
