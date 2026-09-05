de71749153b3fb336e1a1f3ef6345666abc5a701
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-05 10:09:23 +0530

    Create 2553-SeparateTheDigitsInAnArray

---
+++ 2553-SeparateTheDigitsInAnArray/2553-SeparateTheDigitsInAnArray.cpp
@@ -0,0 +1,17 @@
+// Last updated: 05/09/2026, 10:09:23
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            string s = to_string(nums[i]);
+
+            for(char c : s) {
+                ans.push_back(c - '0');
+            }
+        }
+        return ans;
+    }
+};
