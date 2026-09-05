3f0712290f10bb288013584d5744c640e86d3b8e
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-05 10:12:36 +0530

    Create 1306-JumpGameIii

---
+++ 1306-JumpGameIii/1306-JumpGameIii.cpp
@@ -0,0 +1,24 @@
+// Last updated: 05/09/2026, 10:12:36
+class Solution {
+public:
+    int n;
+    bool dfs(vector<int>& arr, int i){
+        if(i < 0 || i >= n || arr[i] < 0){
+            return false;
+        }
+
+        if(arr[i] == 0)
+            return true;
+
+        arr[i] *= -1;
+
+        int left = dfs(arr, i-arr[i]);
+        int right = dfs(arr, i+arr[i]);
+
+        return left || right;
+    }
+    bool canReach(vector<int>& arr, int start) {
+        n = arr.size();
+        return dfs(arr, start);
+    }
+};
