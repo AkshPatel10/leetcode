7108f4bb54ae1ec7bb9f5b8d5442ec4e15dcd37f
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-05 10:10:15 +0530

    Create 1980-FindUniqueBinaryString

---
+++ 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.cpp
@@ -0,0 +1,25 @@
+// Last updated: 05/09/2026, 10:10:14
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        unordered_set<int> st;
+        
+        for(string &num : nums) {
+            st.insert(stoi(num, 0, 2));
+        }
+        
+        int n = nums.size();
+        
+        string result= "";
+        
+        for(int number = 0; number <= 65536; number++) {
+            if(st.find(number) == st.end()) {
+                result = bitset<16>(number).to_string();
+                break;
+            }
+        }
+        
+        return result.substr(16-n);
+        
+    }
+};
