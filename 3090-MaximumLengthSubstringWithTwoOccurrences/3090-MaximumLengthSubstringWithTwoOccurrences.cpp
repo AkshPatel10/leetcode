e3dc16a30e6736354c70bcff3a4049173aa03028
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-05 10:08:31 +0530

    Create 3090-MaximumLengthSubstringWithTwoOccurrences

---
+++ 3090-MaximumLengthSubstringWithTwoOccurrences/3090-MaximumLengthSubstringWithTwoOccurrences.cpp
@@ -0,0 +1,23 @@
+// Last updated: 05/09/2026, 10:08:30
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        unordered_map<char, int> freq;
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+
+            freq[s[right]]++;
+
+            while (freq[s[right]]>2) {
+                freq[s[left]]--;
+                left++;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+        return ans;
+    }
+};
