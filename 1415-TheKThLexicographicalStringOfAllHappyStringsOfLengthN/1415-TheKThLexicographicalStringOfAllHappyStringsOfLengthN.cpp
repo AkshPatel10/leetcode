0d571b9143fb82dc142adda5fa8e23da813ff4cf
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-05 10:12:12 +0530

    Create 1415-TheKThLexicographicalStringOfAllHappyStringsOfLengthN

---
+++ 1415-TheKThLexicographicalStringOfAllHappyStringsOfLengthN/1415-TheKThLexicographicalStringOfAllHappyStringsOfLengthN.cpp
@@ -0,0 +1,37 @@
+// Last updated: 05/09/2026, 10:12:11
+class Solution {
+public:
+    void solve(int n, string curr, int &count, int &k, string& result){
+        if(curr.length()==n){
+            count++;
+            if(count == k){
+                result = curr;
+            }
+            return;
+        }
+
+        for(char ch = 'a'; ch <= 'c'; ch++){
+            if(!curr.empty() && curr.back()==ch)
+                continue;
+
+                curr.push_back(ch);
+
+                solve(n, curr, count, k, result);
+
+                curr.pop_back();
+        }
+
+    }
+
+    string getHappyString(int n, int k) {
+        string curr = "";
+
+        string result = "";
+
+        int count = 0;
+
+        solve(n, curr, count, k, result);
+
+        return result;
+    }
+};
