44f4c3674e3ee365958c5461dc709dc3eaa4a17b
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-05 10:15:01 +0530

    Create 18-4sum

---
+++ 18-4sum/18-4sum.cpp
@@ -0,0 +1,58 @@
+// Last updated: 05/09/2026, 10:15:01
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        for (int i = 0; i < n - 3; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                int l = j + 1;
+                int r = n - 1;
+
+                while (l < r) {
+
+                    long long sum =
+                        (long long)nums[i] + nums[j] +
+                        nums[l] + nums[r];
+
+                    if (sum == target) {
+
+                        ans.push_back({
+                            nums[i], nums[j],
+                            nums[l], nums[r]
+                        });
+
+                        int leftValue = nums[l];
+                        int rightValue = nums[r];
+
+                        while (l < r && nums[l] == leftValue)
+                            l++;
+
+                        while (l < r && nums[r] == rightValue)
+                            r--;
+                    }
+                    else if (sum < target) {
+                        l++;
+                    }
+                    else {
+                        r--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
