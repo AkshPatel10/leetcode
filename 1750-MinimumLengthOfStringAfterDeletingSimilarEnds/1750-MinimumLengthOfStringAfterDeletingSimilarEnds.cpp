c0621696cf1cd6244399087d2358b6badc5193a1
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-05 10:11:05 +0530

    Create 1750-MinimumLengthOfStringAfterDeletingSimilarEnds

---
+++ 1750-MinimumLengthOfStringAfterDeletingSimilarEnds/1750-MinimumLengthOfStringAfterDeletingSimilarEnds.cpp
@@ -0,0 +1,22 @@
+// Last updated: 05/09/2026, 10:11:04
+class Solution {
+public:
+    int minimumLength(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right && s[left] == s[right]) {
+
+            char ch = s[left];
+
+            while (left <= right && s[left] == ch) {
+                left++;
+            }
+
+            while (left <= right && s[right] == ch) {
+                right--;
+            }
+        }
+        return right-left+1;
+    }
+};
