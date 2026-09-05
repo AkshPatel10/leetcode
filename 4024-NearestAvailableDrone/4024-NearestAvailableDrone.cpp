07c48f5c656ecc03151d1a6a984d783b75ca2e67
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-05 10:06:08 +0530

    Create 4024-NearestAvailableDrone

---
+++ 4024-NearestAvailableDrone/4024-NearestAvailableDrone.cpp
@@ -0,0 +1,23 @@
+// Last updated: 05/09/2026, 10:06:08
+class Solution {
+public:
+    int nearestDrone(vector<vector<int>>& drones, vector<int>& target) {
+
+        long long minDist = LLONG_MAX;
+        int ans = -1;
+        
+        for(int i = 0; i < drones.size(); i++){
+            int x = drones[i][0];
+            int y = drones[i][1];
+            int range = drones[i][2];
+
+            long long int dist = abs((long long)x-target[0]) + abs((long long)y-target[1]);
+
+            if(dist <= range && dist < minDist){
+                minDist = dist;
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};
