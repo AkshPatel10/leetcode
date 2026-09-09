3d5d2c01d94c2e9571354df5a6c0c16ef087a952
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-09 13:07:29 +0530

    Create 3870-CountCommasInRange

---
+++ 3870-CountCommasInRange/3870-CountCommasInRange.cpp
@@ -0,0 +1,21 @@
+// Last updated: 09/09/2026, 13:07:28
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+
+        if (n >= 1000) {
+            ans += n - 1000 + 1;
+        }
+
+        if (n >= 1000000) {
+            ans += (n - 1000000 + 1);
+        }
+
+        if (n >= 1000000000) {
+            ans += (n - 1000000000 + 1);
+        }
+
+        return ans;
+    }
+};
