964a0f6e70481b4e68555b7ff0c00128caf60d5d
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-13 12:42:11 +0530

    File updated at 13/09/2026, 12:42:11

--- 136-SingleNumber/136-SingleNumber.cpp
+++ 136-SingleNumber/136-SingleNumber.cpp
@@ -1,11 +1,11 @@
-// Last updated: 17/08/2026, 09:31:16
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int val : nums){
-            ans = ans ^ val;
-        }
-        return ans;
-    }
-};
+// Last updated: 13/09/2026, 12:42:11
+1class Solution {
+2public:
+3    int singleNumber(vector<int>& nums) {
+4        int ans = 0;
+5        for(int val : nums){
+6            ans = ans ^ val;
+7        }
+8        return ans;
+9    }
+10};
