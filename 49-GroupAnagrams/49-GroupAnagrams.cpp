723e3432b27ca39a2dc35698f7b248425641df87
Aksh Patel <128889808+AkshPatel10@users.noreply.github.com>
2026-09-22 20:13:54 +0530

    File updated at 22/09/2026, 20:13:54

--- 49-GroupAnagrams/49-GroupAnagrams.cpp
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,20 +1,20 @@
-// Last updated: 17/08/2026, 09:32:07
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        vector<vector<string>> ans;
-
-        for (int i = 0; i < strs.size(); i++) {
-            string key = strs[i];
-            sort(key.begin(), key.end());
-            mp[key].push_back(strs[i]);
-        }
-
-        for (auto pair : mp) {
-             ans.push_back(pair.second);
-        }
-
-        return ans;
-    }
-};
+// Last updated: 22/09/2026, 20:13:54
+1class Solution {
+2public:
+3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+4        unordered_map<string, vector<string>> mp;
+5        vector<vector<string>> ans;
+6
+7        for (int i = 0; i < strs.size(); i++) {
+8            string key = strs[i];
+9            sort(key.begin(), key.end());
+10            mp[key].push_back(strs[i]);
+11        }
+12
+13        for (auto pair : mp) {
+14             ans.push_back(pair.second);
+15        }
+16
+17        return ans;
+18    }
+19};
